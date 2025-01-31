f9209ddccdbe429dead38d041adb8680567365c1
= <=>
2025-01-31 21:24:48 +0700

    update lagi guys

--- src/app/dashboard/website/tampilan/tampilan.component.ts
+++ src/app/dashboard/website/tampilan/tampilan.component.ts
@@ -8,11 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class TampilanComponent implements OnInit {
 
   cards = [
-    { label: 'Scroll', title: 'Modern', image: 'assets/modern.svg', isActive: true },
-    { label: 'Slide', title: 'Blue', image: 'assets/modern.svg', isActive: false },
-    { label: 'Mobile', title: 'Minimalist', image: 'assets/modern.svg', isActive: false },
-    { label: 'Scroll', title: 'Pinky', image: 'assets/modern.svg', isActive: false },
-    { label: 'Mobile', title: 'Elegant', image: 'assets/modern.svg', isActive: false },
+    { label: 'Scroll', title: 'Modern', image: '/assets/modern.svg', isActive: true },
+    { label: 'Slide', title: 'Blue', image: '/assets/modern.svg', isActive: false },
+    { label: 'Mobile', title: 'Minimalist', image: '/assets/modern.svg', isActive: false },
+    { label: 'Scroll', title: 'Pinky', image: '/assets/modern.svg', isActive: false },
+    { label: 'Mobile', title: 'Elegant', image: '/assets/modern.svg', isActive: false },
   ];
 
   constructor() { }
